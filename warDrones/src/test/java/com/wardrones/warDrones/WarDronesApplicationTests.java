5cae9f02fba77cc34233254c498a19a7fac53607
Gonzalo Briosso <gonzabl0202@gmail.com>
2026-02-11 19:18:20 -0300

    Config inicial de springboot

---
+++ warDrones/src/test/java/com/wardrones/warDrones/WarDronesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wardrones.warDrones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarDronesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
