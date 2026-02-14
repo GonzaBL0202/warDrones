f3727c3b0eca4a992b42e1cd02ad7f649d55252e
Gonzalo Briosso <gonzabl0202@gmail.com>
2026-02-14 15:55:10 -0300

    Se agregan entidades dron y portadron

---
+++ warDrones/src/main/java/com/wardrones/warDrones/model/entity/Dron.java
@@ -0,0 +1,72 @@
+package com.wardrones.warDrones.model.entity;
+
+import com.wardrones.warDrones.model.enums.Bando;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Dron {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int dronId;
+
+    @ManyToOne
+    @JoinColumn(name = "dronportadron_id", nullable = false)
+    private Portadron dronPortaDronId;
+
+    @Enumerated(EnumType.STRING)
+    private Bando dronTipo;
+
+    private int dronPosicionX;
+    private int dronPosicionY;
+    private boolean dronEstado;
+    private int dronVida;
+    private int dronMunicion;
+    private int dronRecargas;
+
+
+    // Getters
+    public int getId() {
+        return dronId;
+    }
+
+    public Portadron getPartidaId() {
+        return dronPortaDronId;
+    }
+
+    public Bando getTipo() {
+        return dronTipo;
+    }
+
+    public int getPosicionX() {
+        return dronPosicionX;
+    }
+
+    public int getPosicionY() {
+        return dronPosicionY;
+    }
+
+    public boolean getEstado() {
+        return dronEstado;
+    }
+
+    public int getVida(){
+        return dronVida;
+    }
+
+    public int getMunicion(){
+        return dronMunicion;
+    }
+
+    public int getRecargas(){
+        return dronRecargas;
+    }
+}
---
+++ warDrones/src/main/java/com/wardrones/warDrones/model/entity/Portadron.java
@@ -0,0 +1,62 @@
+package com.wardrones.warDrones.model.entity;
+
+import com.wardrones.warDrones.model.enums.Bando;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Portadron {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int portadronId;
+
+    @ManyToOne
+    @JoinColumn(name = "portadronpartida_id", nullable = false)
+    private Partida portadronPartidaId;
+
+    @Enumerated(EnumType.STRING)
+    private Bando portadronTipo;
+
+    private int portadronPosicionX;
+    private int portadronPosicionY;
+    private boolean portadronEstado;
+    private int portadronVida;
+
+
+    // Getters
+    public int getId() {
+        return portadronId;
+    }
+
+    public Partida getPartidaId() {
+        return portadronPartidaId;
+    }
+
+    public Bando getTipo() {
+        return portadronTipo;
+    }
+
+    public int getPosicionX() {
+        return portadronPosicionX;
+    }
+
+    public int getPosicionY() {
+        return portadronPosicionY;
+    }
+
+    public boolean getEstado() {
+        return portadronEstado;
+    }
+
+    public int getVida(){
+        return portadronVida;
+    }
+}
